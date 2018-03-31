4fa9917d77ebc5e3f4c202ab23671d93d8da841a
krecelloalex <34172272+krecelloalex@users.noreply.github.com>
2018-03-31 16:19:54 +0300

    real time

---
+++ date_time.js
@@ -0,0 +1,29 @@
+function date_time(id)
+{
+         date = new Date;
+        year = date.getFullYear();
+        month = date.getMonth();
+        months = new Array('', '', '', '', '', '', '', '', '', '', '', '');
+        d = date.getDate();
+        day = date.getDay();
+        days = new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat');
+        h = date.getHours();
+        if(h<10)
+        {
+                h = "0"+h;
+        }
+        m = date.getMinutes();
+        if(m<10)
+        {
+                m = "0"+m;
+        }
+        s = date.getSeconds();
+        if(s<10)
+        {
+                s = "0"+s;
+        }
+        result = ''+days[day]+' '+h+':'+m;
+        document.getElementById(id).innerHTML = result;
+        setTimeout('date_time("'+id+'");','1000');
+        return true;
+}
